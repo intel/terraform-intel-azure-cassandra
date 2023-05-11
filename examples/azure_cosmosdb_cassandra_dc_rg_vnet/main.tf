d9083f6eab76e111bf65a900aadc83aa15f890df
daveshrestha-intel <dave.shrestha@intel.com>
2023-05-11 13:18:45 -0700

    updated with Github links

--- examples/azure_cosmosdb_cassandra_dc_rg_vnet/main.tf
+++ examples/azure_cosmosdb_cassandra_dc_rg_vnet/main.tf
@@ -1,7 +1,7 @@
 ## This module deploys a Azure Managed Instance Cassandra datacter cluster in a user provided resource group, an existing vNET and an Subnet
 #this is exampel for existing azure resource group and vnet
 module "azure-acc-datacenter" {
-  source                = "../../"
+  source                = "intel/azure-cassandra/intel"
   acc_pswd              = var.acc_pswd
   resource_group_name   = "DS-MANAGED-CASS"
   acc_virtualnetwork    = "dscassandra_vnet"
